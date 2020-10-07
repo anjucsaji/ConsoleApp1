c848122e9fede3efeb76daf44601ae0eea48404b
anjucsaji <71403833+anjucsaji@users.noreply.github.com>
2020-10-07 13:45:45 -0400

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args) {
+            string name;
+            int age;
+            int ans = 6 & 3;
+            Console.WriteLine(ans);
+            Console.WriteLine("Hello World");
+            Console.WriteLine("What is ur name");
+            name= Console.ReadLine();
+            Console.WriteLine("\b The User's name is : " + name);
+            Console.WriteLine("What is the user's age");
+            age= Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine("The User's age is: ");
+         
+            Console.WriteLine(Math.Max(2,3));
+
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadLine();
+        }
+      
+    }
+}
